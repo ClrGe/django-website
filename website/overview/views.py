96260713e623e1071ef504012703b6ce6b08ab2c
Lokda-spec <gouarnec@gmail.com>
2022-02-10 13:22:29 +0100

    Commit

---
+++ website/overview/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def hello(request):
+    return HttpResponse('<h1>Hello Django!</h1>')
+
+def about(request):
+    return HttpResponse('<h1>À propos</h1> <p>Nous adorons merch !</p>')
